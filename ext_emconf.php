ff93eaf76ed1991d5a4a871129e84b96bd5d30df
Ernedin Zajko <ezajko@root.ba>
2026-01-15 22:09:34 +0100

    feat: Add grid customization fields, news templates (Horizontal/Text Cards) and fix header rendering

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Spark Integrations',
+    'description' => 'Integrations for external extensions',
+    'category' => 'misc',
+    'author' => 'Ernedin Zajko',
+    'author_email' => 'ezajko@root.ba',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:spark_int/Configuration/TsConfig/Page/News.tsconfig"'
+);
